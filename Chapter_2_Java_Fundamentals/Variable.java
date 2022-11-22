54faeae5b295cd32685194402d6627d2b120019f
Md. Ishtiak Ahmed <42693745+ishtiakrongon@users.noreply.github.com>
2022-11-22 15:29:38 +0600

    variables.java

---
+++ Chapter_2_Java_Fundamentals/Variable.java
@@ -0,0 +1,18 @@
+//2.3 Variables and Literals
+
+// A variable is a names storage location in the computer's memory.
+// A literal is a value that is written into the code of a program.
+
+package Chapter_2_Java_Fundamentals;
+
+public class Variable {
+    public static void main(String[] args) {
+
+        int value;
+
+        value = 5;
+        System.out.print("The value is ");
+        System.out.println(value);
+
+    }
+}
