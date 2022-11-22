6779707396abb84eb9ddc80cb56a7c132ef53331
Md. Ishtiak Ahmed <42693745+ishtiakrongon@users.noreply.github.com>
2022-11-22 15:04:25 +0600

    twolines.java

---
+++ Chapter_2_Java_Fundamentals/TwoLines.java
@@ -0,0 +1,9 @@
+package Chapter_2_Java_Fundamentals;
+
+public class TwoLines {
+    public static void main(String[] args)
+    {
+        System.out.println("Programming is great fun!");
+        System.out.println("I can't get enough of it");
+    }
+}
