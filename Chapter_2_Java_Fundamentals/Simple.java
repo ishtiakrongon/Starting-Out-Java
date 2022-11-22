85b377d1d56d239ab153aff96d309a35b0213df6
Md. Ishtiak Ahmed <42693745+ishtiakrongon@users.noreply.github.com>
2022-11-22 14:59:59 +0600

    initialize

---
+++ Chapter_2_Java_Fundamentals/Simple.java
@@ -0,0 +1,11 @@
+// This is a simple Java Program
+// Learning Basics of Java
+
+package Chapter_2_Java_Fundamentals;
+
+public class Simple {
+    public static void main (String[] args)
+    {
+        System.out.println("Java programming is hard!");
+    }
+}
