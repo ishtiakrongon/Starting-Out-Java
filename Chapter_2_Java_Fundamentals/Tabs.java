a4e3fc23e2a9385be5d180878abbfdb99e27507e
Md. Ishtiak Ahmed <42693745+ishtiakrongon@users.noreply.github.com>
2022-11-22 15:15:29 +0600

    tabs.java

---
+++ Chapter_2_Java_Fundamentals/Tabs.java
@@ -0,0 +1,10 @@
+package Chapter_2_Java_Fundamentals;
+
+public class Tabs {
+    public static void main(String[] args)
+    {
+        System.out.print("These are top sellers:\n");
+        System.out.print("\tComputer games\n \tCoffee\n");
+        System.out.print("\tAspirin");
+    }
+}
