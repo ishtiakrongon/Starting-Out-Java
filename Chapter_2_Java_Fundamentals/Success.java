5d0b0c214b4c8f35fe07f2c4e47bea30d664d3f6
Md. Ishtiak Ahmed <42693745+ishtiakrongon@users.noreply.github.com>
2022-11-22 15:20:57 +0600

    success.java
    problem 2.7

---
+++ Chapter_2_Java_Fundamentals/Success.java
@@ -0,0 +1,11 @@
+package Chapter_2_Java_Fundamentals;
+
+public class Success {
+    public static void main(String[] args)
+    {
+        System.out.print("Success\n");
+        System.out.print("Success ");
+        System.out.print("Success\n");
+        System.out.print("\nSuccess");
+    }
+}
