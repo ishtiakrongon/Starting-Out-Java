0a4794f6d485d8f4b1b215a6508224c6931d0ce0
Md. Ishtiak Ahmed <42693745+ishtiakrongon@users.noreply.github.com>
2022-11-22 15:11:20 +0600

    tabs.java

---
+++ Chapter_2_Java_Fundamentals/Adjusted.java
@@ -0,0 +1,10 @@
+package Chapter_2_Java_Fundamentals;
+
+public class Adjusted {
+    public static void main(String[] args)
+    {
+        System.out.print("These are our top sellers:\n");
+        System.out.print("Computer games\nCoffee\n");
+        System.out.println("Aspirin");
+    }
+}
