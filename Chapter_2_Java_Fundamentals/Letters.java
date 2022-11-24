79dd4208183c315aa1abad715dd79fbf65193e5b
Md. Ishtiak Ahmed <42693745+ishtiakrongon@users.noreply.github.com>
2022-11-24 13:00:44 +0600

    Letters.java

---
+++ Chapter_2_Java_Fundamentals/Letters.java
@@ -0,0 +1,17 @@
+package Chapter_2_Java_Fundamentals;
+
+public class Letters {
+    
+    public static void main(String[] args) {
+
+        char letter;
+        
+        letter = 'A';
+        System.out.println(letter);
+
+        letter = 'B';
+        System.out.println(letter);
+        
+    }
+
+}
