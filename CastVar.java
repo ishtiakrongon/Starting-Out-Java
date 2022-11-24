da9190c192a514bc848ce35b7833a56414625db9
Md. Ishtiak Ahmed <42693745+ishtiakrongon@users.noreply.github.com>
2022-11-24 15:37:22 +0600

    cast var

---
+++ CastVar.java
@@ -0,0 +1,13 @@
+public class CastVar {
+    
+    public static void main(String[] args) {
+
+        int pies = 10, people = 4;
+        double piesPerPerson;
+        piesPerPerson = (double)pies / people;
+
+        System.out.println(piesPerPerson);
+
+    }
+
+}
