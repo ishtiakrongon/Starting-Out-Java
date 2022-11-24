cc8ff4e1215d3dd0555778f91c155f2c27438ace
Md. Ishtiak Ahmed <42693745+ishtiakrongon@users.noreply.github.com>
2022-11-24 21:38:49 +0600

    Average Score

---
+++ Chapter_2_Java_Fundamentals/AverageScore.java
@@ -0,0 +1,52 @@
+package Chapter_2_Java_Fundamentals;
+
+import java.util.Scanner;
+
+public class AverageScore {
+    
+    public static void main (String[] args) {
+
+        double score1;      // To hold score #1
+        double score2;      // To hold score #2
+        double score3;      // To hold score #3
+        double average;     // To hold the average score
+        String input;       // To hold the user's input
+
+        // Creating a scanner object to take input
+        Scanner keyboard = new Scanner(System.in);
+
+        // Get the first test score
+        System.out.println("Enter Score #1: ");
+        score1 = keyboard.nextDouble();
+
+        // Get the second test score
+        System.out.println("Enter Score #2: ");
+        score2 = keyboard.nextDouble();
+
+        // Get the third score
+        System.out.println("Enter Score #3: ");
+        score3 = keyboard.nextDouble();
+
+        // Calculate the average score
+        average = (score1 + score2 + score3) / 3.0;
+
+        // Display the average score
+        System.out.println("The average is " + average);
+
+        // If the score was greater than 95. let the user know
+        // that's a great score.
+
+        input = "That's a great score";
+
+        if (average > 95) {
+
+            System.out.println(input);
+
+        }
+        else {
+
+            System.out.println("Good Work!");
+        }
+    }
+
+}
