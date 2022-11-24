be78a648b9063351823508680c271c6bf193dd64
Md. Ishtiak Ahmed <42693745+ishtiakrongon@users.noreply.github.com>
2022-11-24 11:59:46 +0600

    boolean

---
+++ Chapter_2_Java_Fundamentals/TrueFalse.java
@@ -0,0 +1,17 @@
+package Chapter_2_Java_Fundamentals;
+
+public class TrueFalse {
+    public static void main(String[] args) {
+
+        // A program demonstrating boolean variables
+
+        boolean bool; // deaclaration of boolean variable
+
+        bool = true;
+        System.out.println(bool);
+        
+        bool = false;
+        System.out.println(bool);
+        
+    }
+}
