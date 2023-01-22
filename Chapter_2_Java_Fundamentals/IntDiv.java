f8f6fec7e990f86245a6334b83b7d0bbd60451eb
Md. Ishtiak Ahmed <ishtiak.rongon@gmail.com>
2023-01-22 17:25:17 +0600

    Create IntDiv.java

---
+++ Chapter_2_Java_Fundamentals/IntDiv.java
@@ -0,0 +1,12 @@
+package Chapter_2_Java_Fundamentals;
+
+public class IntDiv {
+    
+    public static void main(String[] args)
+    {
+        double number;
+        number = 5 / 2;
+        System.out.println(number);
+    }
+
+}
