901d1d69293f08fc92b4040e2a431c85fc131616
Md. Ishtiak Ahmed <ishtiak.rongon@gmail.com>
2023-01-22 19:16:16 +0600

    Create city.java

---
+++ Chapter_2_Java_Fundamentals/city.java
@@ -0,0 +1,14 @@
+package Chapter_2_Java_Fundamentals;
+
+public class city {
+    
+    public static void main(String[] args) {
+
+        String city;
+        city = "San Francisco";
+        System.out.print(city);
+
+    }
+
+
+}
