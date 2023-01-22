b2a41f2c4b646183bb4142d226c96beb1b520a6f
Md. Ishtiak Ahmed <ishtiak.rongon@gmail.com>
2023-01-22 15:54:07 +0600

    create a new java file
    Variable declaring in java

---
+++ Chapter_2_Java_Fundamentals/Var_java.java
@@ -0,0 +1,15 @@
+package Chapter_2_Java_Fundamentals;
+
+public class Var_java {
+    
+    public static void main(String[] args) {
+
+        int value;
+
+        value = 5;
+        System.out.print("The Value is: ");
+        System.out.println(value);
+
+    }
+
+}
