47a6e65782911af0b64e6b8cc9b5978d6500326e
Md. Ishtiak Ahmed <ishtiak.rongon@gmail.com>
2023-01-22 16:52:28 +0600

    Create SunFacts.java

---
+++ Chapter_2_Java_Fundamentals/SunFacts.java
@@ -0,0 +1,18 @@
+package Chapter_2_Java_Fundamentals;
+
+// This program uses E notation
+
+public class SunFacts {
+    
+    public static void main(String[] args)
+    {
+        double distance, mass;
+
+        distance = 1.4849654E11;
+        mass = 1.2566E30;
+        System.out.println("The sun is " + distance +
+                                            " meters away");
+        System.out.println("The sun's mass is " + mass + " kilograms.");
+    }
+
+}
