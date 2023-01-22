9a5456045acfa857e1d006dc6ab6169772e8c0a7
Md. Ishtiak Ahmed <ishtiak.rongon@gmail.com>
2023-01-22 15:58:24 +0600

    Create Literals.java

---
+++ Chapter_2_Java_Fundamentals/Literals.java
@@ -0,0 +1,17 @@
+package Chapter_2_Java_Fundamentals;
+
+// A literal is a value that is written i nthe code of a program
+// Literals are commonly assigned to variables or displayed.
+
+public class Literals {
+    
+    public static void main(String[] args) {
+
+        int apples;
+
+        apples = 20;
+        System.out.println("Today we sold " + apples + " bushes of apples.");
+
+    }
+
+}
