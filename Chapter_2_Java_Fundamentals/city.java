f41e67f452b8ee7b63cfe1dcbe1b32464dcaa696
Md. Ishtiak Ahmed <ishtiak.rongon@gmail.com>
2023-01-22 19:23:13 +0600

    Update city.java

--- Chapter_2_Java_Fundamentals/city.java
+++ Chapter_2_Java_Fundamentals/city.java
@@ -6,7 +6,15 @@ public class city {
 
         String city;
         city = "San Francisco";
-        System.out.print(city);
+        int StringSize = city.length();
+        char letter = city.charAt(0);
+        String UpperCity = city.toUpperCase();
+        String LowerCity = city.toLowerCase();
+        System.out.println(city);
+        System.out.println(StringSize);
+        System.out.println(letter);
+        System.out.println(UpperCity);
+        System.out.println(LowerCity);
 
     }
 
