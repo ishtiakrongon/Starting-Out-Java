0c5fa77301c5373b97ead9752063972299b0b711
Md. Ishtiak Ahmed <ishtiak.rongon@gmail.com>
2023-01-22 19:12:49 +0600

    Create TypeCasting.java

---
+++ Chapter_2_Java_Fundamentals/TypeCasting.java
@@ -0,0 +1,14 @@
+package Chapter_2_Java_Fundamentals;
+
+public class TypeCasting {
+    
+    public static void main(String[] args) {
+
+        int pies = 10, people = 4;
+        double PiesPerPerson;
+        PiesPerPerson = (double)pies / people;
+        System.out.println(PiesPerPerson);
+
+    }
+
+}
