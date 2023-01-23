58bee475b8bbe95d1f66327ffa902f35851e0dda
Md. Ishtiak Ahmed <ishtiak.rongon@gmail.com>
2023-01-23 12:12:10 +0600

    Update loanQualifier.java

--- Chapter_3_Conditional_Statements/loanQualifier.java
+++ Chapter_3_Conditional_Statements/loanQualifier.java
@@ -1,50 +1,36 @@
 package Chapter_3_Conditional_Statements;
 import java.util.Scanner;
 
-/**
-    This program demonstrates a nested if statement.
- */
-
-public class loanQualifier {
+public class LoanQualifier {
     
-    public static void main (String[] args) {
-
-        double salary, yearsOnJob;
-
-        // Creating Scanner object
+    public static void main(String[] args)
+    {
+        int salary, yearsOnJob;
         Scanner keyboard = new Scanner(System.in);
 
-        // Get the user's annual salary
-        System.out.print("Enter your Salary: ");
-        salary = keyboard.nextDouble();
+        // get the input of salary
+        System.out.print("Enter your salary: ");
+        salary = keyboard.nextInt();
 
-        // Get the number of years at the current job.
-        System.out.print("Enter the number of years " +
-                            "of your current job: ");
-        yearsOnJob = keyboard.nextDouble();
+        // get the total years of experience in work
+        System.out.print("Enter your work experience in years: ");
+        yearsOnJob = keyboard.nextInt();
 
-        // Determine whether the user qualifies for the loan.
-        if (salary >= 30000) {
+        // Close the input
+        keyboard.close();
 
-            if (yearsOnJob >= 2) {
-
-                System.out.println("You are qualified for the loan.");
-
-            } else {
-
-                System.out.println("You must have been " +
-                                    "on your current job for at least " +
-                                    "two years to qualify.");
-
+        // Nested if-else statements
+        if(salary >= 30000)
+        {
+            if(yearsOnJob >= 2)
+            {
+                System.out.println("You qualify for the loan.");
             }
-
-        } else {
-
-            System.out.println("You must earn at least " +
-                                "$30,000 per year to qualify.");
-
+            else
+            {
+                System.out.print("You must have been on your current job for at least two years to qualify.");    
+            }
         }
-
     }
 
 }
