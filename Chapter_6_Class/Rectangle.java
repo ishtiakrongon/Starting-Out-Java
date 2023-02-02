68437de78b5608917fdf1f1b17a165311db0d172
Md. Ishtiak Ahmed <ishtiak.rongon@gmail.com>
2023-02-02 14:36:02 +0600

    class and methods

---
+++ Chapter_6_Class/Rectangle.java
@@ -0,0 +1,20 @@
+package Chapter_6_Class;
+
+public class Rectangle {
+    
+    private double length;
+    private double width;
+
+    /*
+     * The setLength method stores a value in the length field.
+     * @parameter len The value to store in length.
+     */
+
+    // Access Specifier | Return Type | Method Name
+    public void setLength(double len) {
+
+        length = len;
+
+    }
+
+}
---
+++ LengthDemo.java
@@ -0,0 +1,28 @@
+import Chapter_6_Class.Rectangle;
+
+/*
+ * This program demonstrates the Rectangle class's setLength method.
+ */
+
+public class LengthDemo {
+
+    public static void main(String[] args) {
+        
+        // Create a Rectangle object and assign its
+        // address to the box variables.
+
+        Rectangle box = new Rectangle();
+
+        // Indicate what we are doing.
+        System.out.println("Sending the value 10.0" +
+                            " to the setLength method.");
+        
+        // Call the box object's setLength method.
+        box.setLength(10.0);
+
+        // Indicate we are done.
+        System.out.println("Done.");
+
+    }
+
+}
