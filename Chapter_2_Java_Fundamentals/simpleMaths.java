98bc47c95a17d306066903e2d9c06495e3686e92
Md. Ishtiak Ahmed <ishtiak.rongon@gmail.com>
2023-02-08 10:41:31 +0600

    Create simpleMaths.java

---
+++ Chapter_2_Java_Fundamentals/simpleMaths.java
@@ -0,0 +1,27 @@
+package Chapter_2_Java_Fundamentals;
+
+public class simpleMaths {
+    
+    public static void main(String[] args) {
+
+        double a;
+        double b;
+        double c;
+
+        a = 15.5;
+        b = 26.5;
+        c = -82.99;
+        // Finding Max values
+
+        double z = Math.max(a, b);
+        double y = Math.sqrt(9);
+        double x = Math.abs(c);
+
+
+        System.out.println(z);
+        System.out.println(y);
+        System.out.println(x);
+
+    }
+
+}
