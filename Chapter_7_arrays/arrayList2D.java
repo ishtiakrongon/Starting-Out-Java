41b3deb687923347538036389549d0ed3c7155d5
Md. Ishtiak Ahmed <ishtiak.rongon@gmail.com>
2023-02-12 12:58:52 +0600

    Create arrayList2D.java

---
+++ Chapter_7_arrays/arrayList2D.java
@@ -0,0 +1,34 @@
+package Chapter_7_arrays;
+import java.util.*;
+
+
+public class arrayList2D {
+    
+    public static void main(String[] args) {
+
+        ArrayList<ArrayList<String>> groceryList = new ArrayList();
+
+        ArrayList<String> backerylist = new ArrayList<String>();
+        backerylist.add("pasta");
+        backerylist.add("garlic bread");
+        backerylist.add("donuts");
+
+        ArrayList<String>  produceList = new ArrayList<String>();
+        produceList.add("tomatoes");
+        produceList.add("zucchini");
+        produceList.add("peppers");
+
+        ArrayList<String> drinkList = new ArrayList<String>();
+        drinkList.add("soda");
+        drinkList.add("coffee");
+        drinkList.add("CocaCola");
+
+        // Add arrays into 2d arrays
+        groceryList.add(backerylist);
+        groceryList.add(produceList);
+        groceryList.add(drinkList);
+
+        System.out.println(groceryList.get(2).get(1));
+    }
+
+}
